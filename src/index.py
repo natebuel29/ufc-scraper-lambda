812afbb2c056b64aa79e8af3573e20d25c57e246
Nate Buel <natebuel29@gmail.com>
2022-07-02 11:25:42 -0500

    initialized serverless framework and removed cdk

--- src/index.py
+++ src/index.py
@@ -1,8 +1,6 @@
 from scrapy.crawler import CrawlerProcess
 from scraper.spiders.future_fight_scraper import UfcFutureFightSpider
 
-# issue resolved with this: https://aws.amazon.com/premiumsupport/knowledge-center/lambda-import-module-error-python/
-
 
 def handler(event, context):
     process = CrawlerProcess()
