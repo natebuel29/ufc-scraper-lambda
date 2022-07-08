90b6ddfb52f1239f7fa3a307ed202ae7885d3c38
Nate Buel <natebuel29@gmail.com>
2022-07-07 22:12:46 -0500

    added some simple logging and created the handlers folder

--- src/handlers/future_fights.py
+++ src/handlers/future_fights.py
@@ -1,12 +1,14 @@
-import scrapy
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from src.scraper.spiders.future_fight_scraper import UfcFutureFightSpider
+import logging
 
 
 def handler(event, context):
+    logging.info(
+        f"Kicking off {context.function_name} with Lambda Request ID {context.aws_request_id}")
     configure_logging()
     settings = get_project_settings()  # settings not required if running
     runner = CrawlerRunner(settings)  # from script, defaults provided
