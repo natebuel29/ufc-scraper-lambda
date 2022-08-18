7d4761a8309eec15c877567ac4fabc6983cc6f8e
Nate Buel <natebuel29@gmail.com>
2022-08-17 22:32:06 -0500

    init for previous_event scraper

---
+++ src/scraper/spiders/previous_event_scraper.py
@@ -0,0 +1,4 @@
+# response.css("tr.b-statistics__table-row a::attr(href)").getall()[2]
+#response.css("table.b-fight-details__table tbody tr::attr(data-link)").getall()
+#red fighter -> response.css("a.b-fight-details__person-link::attr(href)").getall()[0]
+#blue fighter -> response.css("a.b-link::attr(href)").getall()[1] 
